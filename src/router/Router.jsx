5657cc51d0422d1420051c69b8b1a0820386623a
muhsinabdullah <muhsinabdullah1250@gmail.com>
2025-12-15 23:00:06 +0600

    Not found Page  added

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,16 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home";
+import SeatingPlan from "../pages/SeatingPlan";
+import NotFound from "../pages/NotFound";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainLayout />,
+        errorElement: <NotFound/>,
         children:[
         {
             path:'/',
             element: <Home />
+        },
+        {
+            path: "/seating", element: <SeatingPlan />
         }
     ]
     },
